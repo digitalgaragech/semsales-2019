babade1b72db5fe6992ebaefc10878bc5cfa5536
Didier Grand <dg@digitalgarage.ch>
2019-06-22 22:12:45 +0200

    test excerpt length

--- functions.php
+++ functions.php
@@ -233,12 +233,6 @@ update_option('image_default_link_type','');
 
 function get_excerpt(){
 	$excerpt = get_the_content();
-	$excerpt = preg_replace(" ([.*?])",'',$excerpt);
-	$excerpt = strip_shortcodes($excerpt);
-	$excerpt = strip_tags($excerpt);
-	$excerpt = substr($excerpt, 0, 20);
-	$excerpt = substr($excerpt, 0, strripos($excerpt, " "));
-	$excerpt = $excerpt.'... <a href="' . get_permalink() . '">Continuer la D lecture</a>';
 	return $excerpt;
 }
 
