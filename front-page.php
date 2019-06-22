691c1f40a668cf51244520f20cfa4b70865f17cc
Didier Grand <dg@digitalgarage.ch>
2019-06-22 22:32:48 +0200

    get excerpt changes

--- front-page.php
+++ front-page.php
@@ -97,7 +97,7 @@ wp_reset_postdata();
           </div>
           <?}?>
           <p>
-            <?php get_excerpt(140, 'content'); ?>
+            <?php get_excerpt(20); ?>
           </p>
           <hr>
         </div>
@@ -130,7 +130,7 @@ wp_reset_postdata();
           </div>
           <?}?>
           <p>
-						<?php get_excerpt(20, 'content'); ?>
+						<?php get_excerpt(20); ?>
           </p>
           <hr>
         <?php endwhile; ?>
@@ -174,7 +174,7 @@ wp_reset_postdata();
           </div>
           <?}?>
           <p>
-            <?php get_excerpt(140, 'content'); ?>
+            <?php get_excerpt(20); ?>
           </p>
           <hr>
         </div>
