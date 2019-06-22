63c3b6d49b01530b6f9163c92682a0be5ff20553
Didier Grand <dg@digitalgarage.ch>
2019-06-22 22:15:14 +0200

    test excerpt lenght

--- functions.php
+++ functions.php
@@ -233,6 +233,12 @@ update_option('image_default_link_type','');
 
 function get_excerpt(){
 	$excerpt = the_content();
+	$excerpt = preg_replace(" ([.*?])",'',$excerpt);
+	$excerpt = strip_shortcodes($excerpt);
+	$excerpt = strip_tags($excerpt);
+	$excerpt = substr($excerpt, 0, 20);
+	$excerpt = substr($excerpt, 0, strripos($excerpt, " "));
+	$excerpt = $excerpt.'... <a href="' . get_permalink() . '">Continuer la D lecture</a>';
 	return $excerpt;
 }
 
