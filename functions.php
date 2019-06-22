06bbc83ccf87019b680613e9ba4a9471709465bf
Didier Grand <dg@digitalgarage.ch>
2019-06-22 22:40:53 +0200

    get excerpt changes

--- functions.php
+++ functions.php
@@ -231,20 +231,15 @@ add_filter('tiny_mce_before_init', 'change_paste_as_text', 1, 2);
 update_option('image_default_link_type','');
 
 
-
-function get_excerpt($limit, $source = null){
-    $excerpt = $source == "content" ? get_the_content() : get_the_excerpt();
-    $excerpt = preg_replace(" (\[.*?\])",'',$excerpt);
-    $excerpt = strip_shortcodes($excerpt);
-    $excerpt = strip_tags($excerpt);
-    $excerpt = substr($excerpt, 0, $limit);
-    $excerpt = substr($excerpt, 0, strripos($excerpt, " "));
-    $excerpt = trim(preg_replace( '/\s+/', ' ', $excerpt));
-    $excerpt = $excerpt.'... <a href="'.get_permalink($post->ID).'">more</a>';
-    return $excerpt;
+function get_excerpt( $count ) {
+  $permalink = get_permalink($post->ID);
+  $excerpt = get_the_content();
+  $excerpt = strip_tags($excerpt);
+  $excerpt = substr($excerpt, 0, $count);
+  $excerpt = substr($excerpt, 0, strripos($excerpt, " "));
+  $excerpt = '<p>'.$excerpt.'... <a href="'.$permalink.'">Read More</a></p>';
+  return $excerpt;
 }
-
-
 
 function add_google_fonts() {
   wp_enqueue_style( ' add_google_fonts ', 'https://fonts.googleapis.com/css?family=Raleway:300', false );
