d34ff8c323e285b23b2ca5ce9b7be4772b99d762
Didier Grand <dg@digitalgarage.ch>
2019-08-19 22:21:45 +0200

    invert image presentation

--- front-page.php
+++ front-page.php
@@ -223,16 +223,16 @@ wp_reset_postdata();
 					$my_query = new WP_Query($args);
 					if( $my_query->have_posts() ) {
 						while ($my_query->have_posts()) : $my_query->the_post(); ?>
+							<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5">
+								<h3><?php the_title(); ?></h3>
+								<?php echo the_content(); ?>
+							</div>
 							<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5 col-lg-offset-1">
 								<?php if( has_post_thumbnail() ) { ?>
 								<div class="homepage-thumb">
 									<?php the_post_thumbnail(); ?>
 								</div>
 								<?}?>
-							</div>
-							<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5">
-								<h3><?php the_title(); ?></h3>
-								<?php echo the_content(); ?>
 							</div>
 							<?php
 						endwhile;
