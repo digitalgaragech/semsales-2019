1ec3db72a03788cf0dd4b398aecd0e6e9d230c16
Didier Grand <dg@digitalgarage.ch>
2019-08-20 21:28:34 +0200

    change excerpt lenght

--- front-page.php
+++ front-page.php
@@ -225,7 +225,7 @@ wp_reset_postdata();
 						while ($my_query->have_posts()) : $my_query->the_post(); ?>
 							<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5 col-lg-offset-1">
 								<h2><?php the_title(); ?></h2>
-								<?php echo get_excerpt(200); ?>
+								<?php echo get_excerpt(400); ?>
 							</div>
 							<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5">
 								<?php if( has_post_thumbnail() ) { ?>
